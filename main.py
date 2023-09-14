b5ad6a1b4e8969b722e86adde2fedafc1750cc96
preethika1525 <144516862+preethika1525@users.noreply.github.com>
2023-09-14 18:38:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-# Factorial of a number using recursion
+# Python program to check if year is a leap year or not
 
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
+year = 2000
 
-num = 7
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
 
-# check if the number is negative
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
 else:
-   print("The factorial of", num, "is", recur_factorial(num))
+    print("{0} is not a leap year".format(year))
