b6613639ddfddf423f8ef77df94296c2a0700011
preethika1525 <144516862+preethika1525@users.noreply.github.com>
2023-09-15 14:24:53 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,17 +1,54 @@
-# Factorial of a number using recursion
+class Batsman:
+    def __init__(self):
+        self.strike_rate = 0.0
+        self.total_runs = 0
+        self.highest_score = 0
+        self.batting_rank = 0
+    def get_bat(self,sr,tr,hs,br):
+        self.strike_rate = sr
+        self.total_runs = tr
+        self.highest_score = hs
+        self.batting_rank = br
+    def disp_bat(self):
+        print ("\nBATTING DATA\n")
+        print ("Strike Rate:"),self.strike_rate
+        print ("Total Runs:"),self.total_runs
+        print ("Highest Score:"),self.highest_score
+        print ("Batting Rank:"),self.batting_rank      
 
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
+class Bowler:
+    def __init__(self):
+        self.wickets_taken = 0
+        self.economy = 0.0
+        self.hattricks = 0
+        self.bowling_rank = 0
+    def get_bowl(self,wt,ec,ht,bor):
+        self.wickets_taken = wt
+        self.economy = ec
+        self.hattricks = ht
+        self.bowling_rank = bor
+    def disp_bowl(self):
+        print ("\nBOWLING DATA\n")
+        print ("Wickets Taken:"),self.wickets_taken
+        print ("Economy:"),self.economy
+        print ("Hattricks:"),self.hattricks
+        print ("Bowling Rank:"),self.bowling_rank
 
-num = 7
+class AllRounder(Batsman,Bowler):
+    def __init__(self):
+        Batsman.__init__(self)
+        Bowler.__init__(self)
+        self.allrounder_rank = 0
+    def get_all(self,sr,tr,hs,br,wt,ec,ht,bor,ar):
+        Batsman.get_bat(self,sr,tr,hs,br)
+        Bowler.get_bowl(self,wt,ec,ht,bor)
+        self.allrounder_rank = ar
+    def disp_all(self):
+        print ("\nALL-ROUNDER DATA")
+        print ("\nAll-Rounder Rank:"),self.allrounder_rank
+        self.disp_bat()
+        self.disp_bowl()
 
-# check if the number is negative
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   print("The factorial of", num, "is", recur_factorial(num))
+player1 = AllRounder()
+player1.get_all(89.7,3024,96,67,101,5.67,4,34,57)
+player1.disp_all()
